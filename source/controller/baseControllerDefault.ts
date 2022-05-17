b80786ca0ed835b63424f066f05e9ec48b21e92f
Judah <judahholanda7@gmail.com>
2022-05-17 19:14:20 -0300

    use toJSON

--- source/controller/baseControllerDefault.ts
+++ source/controller/baseControllerDefault.ts
@@ -1,11 +1,16 @@
 import { Operation } from 'flexiblepersistence';
 import { AbstractControllerDefault } from 'backapi';
+import { toJSON } from 'flatted';
+
 export default class BaseControllerDefault extends AbstractControllerDefault {
   protected emit(responseOrSocket?, operation?: Operation, status?, object?) {
     const returnName =
       this.formatName().charAt(0).toLowerCase() +
       this.formatName().slice(1) +
       (operation === undefined ? '' : '.' + Operation[operation]);
-    return responseOrSocket.emit(returnName, { status, object });
+    return responseOrSocket.emit(returnName, {
+      status,
+      object: toJSON(object),
+    });
   }
 }
